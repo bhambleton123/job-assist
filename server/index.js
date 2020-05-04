2315bc72be43eb458d5af8e269de3d768928fa8e
Brian Hambleton <bhambl3t0n1@gmail.com>
2020-05-04 01:41:41 -0700

    Added some middleware, set up google oauth

--- server/index.js
+++ server/index.js
@@ -1,12 +1,42 @@
+require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
 const scrapers = require("./routes/scrapers");
 const auth = require("./routes/auth");
 const passport = require("./auth/passport");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const redis = require("redis");
+const session = require("express-session");
+const redisClient = redis.createClient();
+const redisStore = require("connect-redis")(session);
+
+redisClient.on("error", (err) => {
+  console.log(`Redis error: ${err}`);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  session({
+    store: new redisStore({
+      host: "localhost",
+      port: 6379,
+      client: redisClient,
+      ttl: 604800,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
+  })
+);
 
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/auth", auth);
 app.use("/jobs", scrapers);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
 const passport = require("../auth/passport");
 
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 router.get("/google/callback", passport.authenticate("google"), (req, res) => {
   res.send(req.user);
+});
+
+router.get("/user", (req, res) =>
+  res.send(req.user ? req.user : { User: "Not logged in" })
+);
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.send("user logged out");
 });
 
 module.exports = router;
