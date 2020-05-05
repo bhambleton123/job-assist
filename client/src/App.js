8f495ed16c5e274a91d4cf7d78f0ecda816a586c
Brian Hambleton <bhambl3t0n1@gmail.com>
2020-05-04 21:42:31 -0700

    Added basic routing for features of application

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Dashboard from "./dashboard/dashboard";
+import JobBoard from "./job-board/job-board";
+import LandingPage from "./landing-page/landing-page";
+import Profile from "./profile/profile";
+import SearchJobs from "./search-jobs/search-jobs";
 
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/job-board" component={JobBoard} />
+        <Route path="/search-jobs" component={SearchJobs} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/" component={LandingPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/job-board/job-board.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function JobBoard() {
+  return <div>Job Board</div>;
+}
