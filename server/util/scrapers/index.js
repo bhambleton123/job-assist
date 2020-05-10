57e6c31c2583482576c80f5086e3f65b8831e7ab
Brian Hambleton <bhambl3t0n1@gmail.com>
2020-05-10 10:54:49 -0700

    Fixed scraper to return learn more text when information can't be found from the link

--- server/util/scrapers/index.js
+++ server/util/scrapers/index.js
@@ -7,15 +7,15 @@ const __launchPuppeteer = async () => {
 
   await avoidDetection(page);
 
-  return page;
+  return { browser, page };
 };
 
 const scrapeIndeed = async (url, pageNum) => {
   let newUrl = `${url}&start=${50 * pageNum}`;
-  const page = await __launchPuppeteer();
-  await page.goto(newUrl);
+  const puppeteer = await __launchPuppeteer();
+  await puppeteer.page.goto(newUrl);
 
-  const jobs = await page.evaluate(() => {
+  const jobs = await puppeteer.page.evaluate(() => {
     const jobLinks = Array.from(
       document.getElementsByClassName("jobtitle")
     ).map((title, index) => {
@@ -30,18 +30,24 @@ const scrapeIndeed = async (url, pageNum) => {
 
     return jobLinks.filter((job) => job.link.split("/")[3] !== "pagead");
   });
+  puppeteer.browser.close();
 
   return jobs;
 };
 
 const scrapeIndeedJobDescription = async (url) => {
-  const page = await __launchPuppeteer();
+  const puppeteer = await __launchPuppeteer();
   url = url.replace("/rc/clk", "/viewjob");
-  await page.goto(url);
+  await puppeteer.page.goto(url);
 
-  const description = await page.evaluate(() => {
-    return document.getElementById("jobDescriptionText").innerHTML;
+  const description = await puppeteer.page.evaluate(() => {
+    if (!document.getElementById("jobDescriptionText")) {
+      return "Click link below to learn more...";
+    } else {
+      return document.getElementById("jobDescriptionText").innerHTML;
+    }
   });
+  puppeteer.browser.close();
   return description;
 };
 
